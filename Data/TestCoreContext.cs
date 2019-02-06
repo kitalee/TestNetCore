be04ca84c2fe1bb1e24a11f066077a2f59e8817b
daniel <alcufk@naver.com>
2019-02-06 16:53:11 +1100

    Scaffolding Movie Model

---
+++ Data/TestCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestCore.Models
+{
+    public class TestCoreContext : DbContext
+    {
+        public TestCoreContext (DbContextOptions<TestCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestCore.Models.Movie> Movie { get; set; }
+    }
+}
