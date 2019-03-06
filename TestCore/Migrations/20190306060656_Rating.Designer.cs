978072918b423c71ff22501d268848d7649992bb
daniel <alcufk@naver.com>
2019-03-06 17:14:33 +1100

    add a new field

--- TestCore/Migrations/20190306060656_Rating.Designer.cs
+++ TestCore/Migrations/20190306060656_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestCore.Models;
 
 namespace TestCore.Migrations
 {
     [DbContext(typeof(TestCoreContext))]
-    partial class TestCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20190306060656_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
